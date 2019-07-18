60c4ae5ccbf1dd5204168c5aebaca24f6f6b9723
athipat87 <35474412+athipat87@users.noreply.github.com>
2019-07-18 18:26:26 +0700

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -4,6 +4,6 @@ $Setup_User = 'athipat_day';
 $Setup_Pwd = 'it!112233';
 $Setup_Database = 'day';
 mysql_connect($Setup_Server,$Setup_User,$Setup_Pwd);
-mysql_query("$Setup_Database");
+mysql_query("use $Setup_Database");
 mysql_query("SET NAMES UTF8");
 ?>
