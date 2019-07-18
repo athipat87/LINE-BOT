6d23dd31fc6497847d5870009d247ffa1ed44a63
athipat87 <35474412+athipat87@users.noreply.github.com>
2019-07-18 18:23:55 +0700

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,8 @@
 <?php
-$Setup_Server = 'localhost';
+$Setup_Server = '188.166.106.66';
 $Setup_User = 'athipat_day';
 $Setup_Pwd = 'it!112233';
-$Setup_Database = 'athipat_linebot';
+$Setup_Database = 'day';
 mysql_connect($Setup_Server,$Setup_User,$Setup_Pwd);
 mysql_query("$Setup_Database");
 mysql_query("SET NAMES UTF8");
